12344a5f371dcd9da5f2262b46e94eb4afcf000a
shipdocs <info@shipdocs.app>
2025-12-30 20:18:05 +0100

    chore: bump version to 2.0.21

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bastion-firewall",
-    version="2.0.20",
+    version="2.0.21",
     description="Bastion Firewall - Application Firewall for Linux",
     author="Bastion Team",
     packages=find_packages(),
