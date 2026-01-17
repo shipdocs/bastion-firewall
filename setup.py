73ced98a737cf7e4069e56f6e41c181bd0bb3351
shipdocs <info@shipdocs.app>
2026-01-17 12:15:04 +0100

    Release v2.0.31

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bastion-firewall",
-    version="2.0.30",
+    version="2.0.31",
     description="Bastion Firewall - Application Firewall for Linux",
     author="Bastion Team",
     packages=find_packages(),
